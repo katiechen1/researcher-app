48b657c3a1b661c01ce1ae9855fa3c2c30d1d604
Katie Chen <katie.chen96@gmail.com>
2018-07-09 11:47:21 -0700

    merge conflict

--- researchers/models.py
+++ researchers/models.py
@@ -8,8 +8,6 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
-
-
 #Researcher objects for each researcher on the page 
 class Researcher(models.Model):
     LEVELS_CHOICES = [('Assistant Professor', 'Assistant Professor'), ('Associate Professor', 'Associate Professor'), ('Full Professor', 'Full Professor'), ('Senior Industry Scientist', 'Senior Industry Scientist')]
@@ -29,55 +27,6 @@ class NominateForm(forms.Form):
     nominators_email = forms.EmailField(required=True)
     nominees_name = forms.CharField(required=True)
     nominees_email = forms.EmailField(required=True)
-
-<<<<<<< HEAD
-# This is the form we send to nominated individual to get information needed to display
-class NominatedInfo(forms.Form):
-    LEVELS_CHOICES = [('Assistant Professor', 'Assistant Professor'), ('Associate Professor', 'Associate Professor'), ('Full Professor', 'Full Professor'), ('Senior Industry Scientist', 'Senior Industry Scientist')]
-    name = forms.CharField(required=True)
-    #EMAIL
-    email = forms.EmailField(required=True)
-    institution = forms.CharField(required=True)
-    position = forms.CharField(required=True)
-    #WEBSITES
-    website = forms.URLField(required=True)
-    linkedin = forms.URLField(required=True)
-
-
-    country = forms.CharField(required=True)
-    level = forms.ChoiceField(choices=LEVELS_CHOICES, required=True)
-    description = forms.CharField(required=True,
-        widget=forms.Textarea(attrs={'cols': 40, 'rows': 5}), max_length=100)
-    class Meta:
-        model = Researcher
-
-=======
->>>>>>> parent of 5e9303f... fixed redirect page
-# # This is the form used to nominvi modeate a scholar or yourself
-# class NominateForm(ModelForm):
-#     nominators_full_name = forms.CharField(required=True)
-#     nominators_email = forms.EmailField(required=True)
-#     nominees_name = forms.CharField(required=True)
-#     nominees_email = forms.EmailField(required=True)
-#     class Meta:
-#         model = Researcher
-#         fields = ('name', 'email')
-#         labels = {
-#             'HELLO': 'nominators_full_name', 'BYE': 'name'
-#         }
-
-# # This is the form we send to nominated individual to get information needed to display
-# class NominatedInfo(forms.Form):
-#     LEVELS_CHOICES = [('AP', 'Assistant Professor'), ('AsCP', 'Associate Professor'), ('FP', 'Full Professor'), ('SIS', 'Senior Industry Scientist')]
-#     name = forms.CharField(required=True)
-#     email = forms.CharField(required=True)
-#     institution = forms.CharField(required=True)
-#     position = forms.CharField(required=True)
-#     website = forms.CharField(required=True)
-#     linkedin = forms.CharField(required=True)
-#     level = forms.ChoiceField(choices=LEVELS_CHOICES, required=True)
-#     description = forms.CharField(required=True,
-#         widget=forms.Textarea, max_length=100)
 
 
 # This is the form we send to nominated individual to get information needed to display
@@ -103,49 +52,6 @@ class NominatedInfo(forms.Form):
 #             _('%(value)s is not at least five words'),
 #             params={'value': value},
 #         )
-
-
-# one nominator can nominate many researchers
-# one to many relationship: foreign key
-
-# #nominator model 
-# class Nominator(models.Model):
-
-#     contact_name = forms.CharField(required=True)
-#     contact_email = forms.EmailField(required=True)
-#     nominee_name = forms.CharField(required=True)
-#     nominee_email = forms.EmailField(required=True)
-#     nominee_website = forms.CharField(required=True)
-#     nominee_institution = forms.CharField(required=True)
-
-
-
-# #nominee model
-# class Nominee(models.Model):
-
-
-#     LEVELS_CHOICES = [('EC', 'Early Career'), ('MS', 'Mid/Senior Career')]
-    
-#     name = forms.CharField(required=True)
-#     email = forms.CharField(required=True)
-#     institution = forms.CharField(required=True)
-#     position = forms.CharField(required=True)
-#     description = forms.CharField(required=True,
-#         widget=forms.Textarea)
-#     website = forms.CharField(required=True)
-#     linkedin = forms.CharField(required=True)
-
-#     level = models.CharField(choices=LEVELS_CHOICES, max_length=200, blank=True)
-
-# #nomination model
-# class Nomination(models.Model):
-#     d_nominator = Nominator()
-#     d_nominee = Nominee() 
-#     STATUS_CHOICES = [('PENDING', 'pending'), ('APPROVED', 'approved')]
-#     nominator = models.ForeignKey(Nominator, on_delete=models.CASCADE)
-#     nominee = models.ForeignKey(Nominee, on_delete=models.CASCADE)
-#     status = models.CharField(choices=STATUS_CHOICES, max_length=1, default='PENDING')
-
 
 
 
